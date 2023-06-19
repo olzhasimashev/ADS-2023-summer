792ac7f72a0d37bb04394ac9c33bfa56c73b488e
Imashev Olzhas <imashev.olzhas@gmail.com>
2023-06-20 02:27:57 +0600

    modified Problem I, time-limit exceeded, test 8

--- lab_1/i.py
+++ lab_1/i.py
@@ -1,17 +1,22 @@
-def elite(n, votes):
-    i = 0
-    while 'S' in votes and 'K' in votes:
-        if votes[i] == 'S':
-            votes = votes.replace('K', '', 1)
-        else:
-            votes = votes.replace('S', '', 1)
-        i += 1
-        print(i)
-        if votes[i] == votes[-1]:
-            i = 0
-        print(votes)
-        
-    if 'S' in votes:
+def find_leader(n, votes):
+    temp_list = list(votes)
+    
+    while len(set(temp_list)) > 1:
+        i = 0
+        while i < len(temp_list):
+            if temp_list[i] == 'S':
+                if 'K' in temp_list[i+1:]:
+                    temp_list = temp_list[:i+1 + temp_list[i+1:].index('K')] + temp_list[i+2 + temp_list[i+1:].index('K'):]
+                elif 'K' in temp_list:
+                    temp_list = temp_list[:temp_list.index('K')] + temp_list[temp_list.index('K')+1:]
+            elif temp_list[i] == 'K':
+                if 'S' in temp_list[i+1:]:
+                    temp_list = temp_list[:i+1 + temp_list[i+1:].index('S')] + temp_list[i+2 + temp_list[i+1:].index('S'):]
+                elif 'S' in temp_list:
+                    temp_list = temp_list[:temp_list.index('S')] + temp_list[temp_list.index('S')+1:]
+            i += 1
+
+    if temp_list[0] == 'S':
         return "SAKAYANAGI"
     else:
         return "KATSURAGI"
@@ -19,4 +24,4 @@ def elite(n, votes):
 n = int(input().strip())
 votes = input().strip()
 
-print(elite(n, votes))
+print(find_leader(n, votes))
